58855be27702424b34ffd55da476a7713af11a1f
selesdepselesnul <selesdepselesnul@gmail.com>
2016-05-08 07:35:35 +0700

    refactor and add view

--- cucokers.py
+++ cucokers.py
@@ -2,33 +2,43 @@
 import re, sys
 
 def select_mode(mode):
-	if mode == '--database':
-		return 'CREATE DATABASE'
-	elif mode == '--table':
-		return 'CREATE TABLE'
+	if mode == '--c-database':
+		return r"CREATE DATABASE"
+	elif mode == '--c-table':
+		return r"CREATE TABLE"
+	elif mode == '--c-view':
+		print('c-view mode')
+		return r"CREATE.+VIEW"
 	else:
-		return ''
+		return ""
 
 
-mode = sys.argv[1]
-source = sys.argv[2]
-dest = sys.argv[3]
+def main():
+	params = {
+		'mode' : sys.argv[1],
+		'src' : sys.argv[2],
+		'dest' : sys.argv[3] 
+	}
 
-in_file = open(source, 'r')
-text_to_split = in_file.read();
-filter_text = text_to_split.split(';')
+	in_file = open(params['src'], 'r')
+	text_to_split = in_file.read();
+	filter_text = text_to_split.split(';')
 
-out_file = open(dest, 'w')
+	out_file = open(params['dest'], 'w')
 
-if len(sys.argv) < 3:
-	print('arg < 3')
-else:
-	if select_mode(mode) != '':
-		for x in filter_text:
-			if select_mode(mode) in x:
-				out_file.write(x+';')
-				print(x+';')
+	if len(sys.argv) < 3:
+		print('arg < 3')
+	else:
+		if select_mode(params['mode']) != '':
+			pattern_reg = select_mode(params['mode'])
+			print(pattern_reg)
+			pattern = re.compile(pattern_reg)
+			for x in filter_text:
+				if pattern.search(x):
+					out_file.write(x+';')
+					print(x+';')	
 
 
-	
+if __name__ == '__main__':
+	main()
 	
